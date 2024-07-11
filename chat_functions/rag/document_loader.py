06f8d3e456aab7e2f103e0afcfb3d7bebdc5814e
Paulo Roberto <paulo.roberto97@live.com>
2024-07-10 21:18:33 -0300

    fix: Correção do nome do atributo, indicando que é privado

--- chat_functions/rag/document_loader.py
+++ chat_functions/rag/document_loader.py
@@ -2,8 +2,8 @@ class DocumentLoader:
     def __init__(self, file_path, file_type):
         if file_type == 'pdf':
             from langchain_community.document_loaders import PyPDFLoader
-            self.carregar_doc = PyPDFLoader(file_path).load_and_split()
+            self._carregar_doc = PyPDFLoader(file_path).load_and_split()
 
 
     def load(self):
-        return self.carregar_doc
+        return self._carregar_doc
