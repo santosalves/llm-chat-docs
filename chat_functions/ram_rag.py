62d15c755190f0b53fd78af95fd735f413654f7c
Paulo Roberto <paulo.roberto97@live.com>
2024-07-13 11:16:49 -0300

    refactor: Criação da classe para abstrair os providers para embeddings. Ajuste nos parâmetros de inferência e envs

--- chat_functions/ram_rag.py
+++ chat_functions/ram_rag.py
@@ -2,22 +2,22 @@ def carregar_docs(nome_do_doc):
     from chat_functions.rag.document_loader import DocumentLoader
     from chat_functions.rag.text_splitter import TextSplitter
     from langchain_community.vectorstores import FAISS
-    from langchain_community.embeddings import OllamaEmbeddings
+    from chat_functions.rag.embeddings import Embeddings
     from chat_functions.config_reader import ConfigReader
-    from os import system
+    from os import system, getenv
     from time import sleep
 
-
-    config = ConfigReader('conf/rag_config.json')
 
     print('\033[92mLendo o DB local...\033[0m')
     carregar_doc = DocumentLoader(f'docs/{nome_do_doc}', f'{nome_do_doc.split(".")[-1]}')
     documentos = carregar_doc.load()
 
-    fragmenta_texto = TextSplitter(chunk_size=1000, chunk_overlap=0)
+    fragmenta_texto = TextSplitter(getenv('CHUNK_SIZE'), getenv('CHUNK_OVERLAP'))
     docs = fragmenta_texto.split_docs(documentos)
 
-    embeddings = OllamaEmbeddings(**config.get_config())
+    config = ConfigReader('conf/rag_config.json')
+    provider = Embeddings(**config.get_config())
+    embeddings = provider.ollama()
     
     while True:
         try:
