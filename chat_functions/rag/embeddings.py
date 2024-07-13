02449ccd8977ab54e1e9f98322b1fe4156ba9266
Paulo Roberto <paulo.roberto97@live.com>
2024-07-13 11:34:57 -0300

    fix: Ajustes de apresentação da classe

--- chat_functions/rag/embeddings.py
+++ chat_functions/rag/embeddings.py
@@ -1,10 +1,8 @@
 class Embeddings:
     def __init__(self, **config):
         self._config = config
-        print(self._config)
 
 
     def ollama(self):
         from langchain_community.embeddings import OllamaEmbeddings
         return OllamaEmbeddings(**self._config)
-
